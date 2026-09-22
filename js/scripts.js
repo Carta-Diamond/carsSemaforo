84c996a19daf24a6802fdb60e74cfcd98dcfabee
Carlotta <juan.rubio262132@potros.itson.edu.mx>
2026-09-22 08:54:23 -0700

    funcion encenderColor del modo manual actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -34,32 +34,12 @@ let rCount = 0
 let yCount = 0
 let gCount = 0
 
-
 function encenderColor(color){
-    if (color.id === "off"){
-        document.querySelector(".red-light").style.backgroundColor = "gray";
-        document.querySelector(".yellow-light").style.backgroundColor = "gray";
-        document.querySelector(".green-light").style.backgroundColor = "gray";
-    }
-    
-    if (color.id === "red-light"){
-        document.querySelector(".red-light").style.backgroundColor = "rgb(251, 3, 3)";
-        document.querySelector(".yellow-light").style.backgroundColor = "gray";
-        document.querySelector(".green-light").style.backgroundColor = "gray";
-        return;
-    }
-    if (color.id === "yellow-light"){
-        document.querySelector(".red-light").style.backgroundColor = "gray";
-        document.querySelector(".yellow-light").style.backgroundColor = "rgb(250, 230, 3)";
-        document.querySelector(".green-light").style.backgroundColor = "gray";
-        return;
-    }
-    if (color.id === "green-light"){
-        document.querySelector(".red-light").style.backgroundColor = "gray";
-        document.querySelector(".yellow-light").style.backgroundColor = "gray";
-        document.querySelector(".green-light").style.backgroundColor = "rgb(18, 188, 29)";
-        return;
-    }
+    rojo.classList.remove('on')
+    amarillo.classList.remove('on')
+    verde.classList.remove('on')
+
+    color.classList.add('on')
 }
 
 btnRojo.addEventListener("click", function(){
@@ -67,9 +47,9 @@ btnRojo.addEventListener("click", function(){
     const color = document.getElementById("red-light")
     const redCount = document.getElementById("redCount")
     encenderColor(color);
-    redCount.textContent = rCount++;
-
+    redCount.textContent = ++rCount + (rCount === 1 ? " vez" : " veces");;
     return;
+
 });
 
 btnAmarillo.addEventListener("click", function(){
@@ -77,7 +57,7 @@ btnAmarillo.addEventListener("click", function(){
     const color = document.getElementById("yellow-light")
     const yellowCount= document.getElementById("yellowCount")
     encenderColor(color)
-    yellowCount.textContent = yCount++;
+    yellowCount.textContent = ++yCount + (yCount === 1 ? " vez" : " veces");;
     return;
 
 });
@@ -87,8 +67,9 @@ btnVerde.addEventListener("click", function(){
     const color = document.getElementById("green-light")
     const greenCount= document.getElementById("greenCount")
     encenderColor(color)
-    greenCount.textContent = gCount++;
+    greenCount.textContent = ++gCount + (gCount === 1 ? " vez" : " veces");;
     return;
+
 });
 
 btnApagar.addEventListener("click", function(){
@@ -96,5 +77,6 @@ btnApagar.addEventListener("click", function(){
     const color = document.getElementById("off")
     encenderColor(color)
     return;
+    
 })
 
